397bbd2a64f9b53358cb100be2bc717b7b0e3b44
Phel <honeydove_mhar@yahoo.com>
2013-03-04 23:32:58 -0800

    Show Sidebar when logged in only, http://motivatedmodels.com/agency/profile-member/

--- theme/member-overview.php
+++ theme/member-overview.php
@@ -24,6 +24,12 @@ $rb_agencyinteract_option_overviewpagedetails 	= (int)$rb_agencyinteract_options
 // Check Sidebar
 $rb_agencyinteract_options_arr = get_option('rb_agencyinteract_options');
 $rb_agencyinteract_option_profilemanage_sidebar = $rb_agencyinteract_options_arr['rb_agencyinteract_option_profilemanage_sidebar'];
+$content_class = "";
+if (is_user_logged_in()) {
+	$content_class = "eight";
+} else {
+	$content_class = "twelve";
+}
 
 // Were they users or agents?
 $profiletype = (int)get_user_meta($current_user->id, "rb_agency_interact_profiletype", true);
@@ -39,11 +45,12 @@ add_filter('wp_title', 'rb_agencyinteractive_override_title', 10, 2);
 get_header();
 	
 	if (is_user_logged_in()) {
+
 		// Show registration steps
 		echo "	<div id=\"profile-steps\">Profile Setup: Step 1 of 4</div>\n";
 	}
 
-	echo "	<div id=\"container\" class=\"eight column rb-agency-interact rb-agency-interact-overview\">\n";
+	echo "	<div id=\"container\" class=\"".$content_class." column rb-agency-interact rb-agency-interact-overview\">\n";
 	echo "  	<div id=\"content\">\n";
 
 		// get profile Custom fields value
@@ -90,36 +97,37 @@ get_header();
 			// No Record Exists, register them
 			} else {
 					
-					echo "<h1>". __("Welcome", rb_agencyinteract_TEXTDOMAIN) ." ". $current_user->first_name ."!</h1>";
+				echo "<h1>". __("Welcome", rb_agencyinteract_TEXTDOMAIN) ." ". $current_user->first_name ."!</h1>";
 
-					if ($profiletype == 1) {
+				if ($profiletype == 1) {
+					echo "". __("We have you registered as", rb_agencyinteract_TEXTDOMAIN) ." <strong>". $profiletypetext ."</strong>";
+					echo "<h2><a href=\"". $rb_agencyinteract_WPURL ."/profile-search/\">". __("Begin Your Search", rb_agencyinteract_TEXTDOMAIN) ."</a></h2>";
+					
+					echo " <div id=\"subscription-customtext\">\n";
+					$Page = get_page($rb_agencyinteract_option_subscribepagedetails);
+					echo apply_filters('the_content', $Page->post_content);
+					echo " </div>";
+
+				} else {
+				  	if ($rb_agencyinteract_option_registerallow == 1) {
+
+						// Users CAN register themselves
 						echo "". __("We have you registered as", rb_agencyinteract_TEXTDOMAIN) ." <strong>". $profiletypetext ."</strong>";
-						echo "<h2><a href=\"". $rb_agencyinteract_WPURL ."/profile-search/\">". __("Begin Your Search", rb_agencyinteract_TEXTDOMAIN) ."</a></h2>";
-						
-						echo " <div id=\"subscription-customtext\">\n";
-						$Page = get_page($rb_agencyinteract_option_subscribepagedetails);
-						echo apply_filters('the_content', $Page->post_content);
-						echo " </div>";
-
-					} else {
-					  	if ($rb_agencyinteract_option_registerallow == 1) {
-
-							// Users CAN register themselves
-							echo "". __("We have you registered as", rb_agencyinteract_TEXTDOMAIN) ." <strong>". $profiletypetext ."</strong>";
-							echo "<h2>". __("Setup Your Profile", rb_agencyinteract_TEXTDOMAIN) ."</h2>";
-						
-							// Register Profile
-							include("include-profileregister.php");						
-					  	} else {
-						
-							// Cant register
-							echo "<strong>". __("Self registration is not permitted.", rb_agencyinteract_TEXTDOMAIN) ."</strong>";
-					  	}
-					}					
+						echo "<h2>". __("Setup Your Profile", rb_agencyinteract_TEXTDOMAIN) ."</h2>";
+					
+						// Register Profile
+						include("include-profileregister.php");						
+				  	} else {
+					
+						// Cant register
+						echo "<strong>". __("Self registration is not permitted.", rb_agencyinteract_TEXTDOMAIN) ."</strong>";
+				  	}
+				}					
 			}			
 			echo "</div><!-- #profile-manage -->\n";
 
 		} else {
+
 			// Show Login Form
 			include("include-login.php"); 	
 		}
@@ -127,11 +135,14 @@ get_header();
 	echo "  </div><!-- #content -->\n";
 	echo "</div><!-- #container -->\n";
 	
-	// Get Sidebar 
-	$LayoutType = "";
-	if ($rb_agencyinteract_option_profilemanage_sidebar) {
-		$LayoutType = "profile";
-		get_sidebar();
+	if (is_user_logged_in()) {
+
+		// Get Sidebar 
+		$LayoutType = "";
+		if ($rb_agencyinteract_option_profilemanage_sidebar) {
+			$LayoutType = "profile";
+			get_sidebar();
+		}
 	}
 
 // Get Footer
