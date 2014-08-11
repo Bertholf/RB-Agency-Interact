cf88ffa109d9d614b04806ad89e5e7e8d0a7e90c
Champ Camba <info@champiewebfolio.com>
2014-08-12 07:31:20 +0800

    Fixed email notification details

--- functions.php
+++ functions.php
@@ -205,7 +205,7 @@
 					$message .= sprintf(__('Congratulations! Your account is approved. Below is your login information.'), $user_login) . "\r\n"; 
 					$message .= sprintf(__("Here's how to log in:"), get_option('blogname')) . "\r\n\r\n"; 
 					$message .= get_option('home') ."/profile-login/\r\n"; 
-					if($rb_agencyinteract_option_registerapproval == 1){
+					if($rb_agencyinteract_option_registerapproval == 0){
 								$message .= sprintf(__('Username: %s'), $user_login) . "\r\n"; 
 								$message .= sprintf(__('Password: %s'),  $user_pass) . "\r\n\r\n"; 
 					}/*else{ // manually approved
